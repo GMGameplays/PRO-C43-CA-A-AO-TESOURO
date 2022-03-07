d8dd390d3614abc39ac2a8f99340cd7e35cc4f91
GMGameplays <46766196+GMGameplays@users.noreply.github.com>
2022-03-07 17:39:12 -0300

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,20 @@
+const accessCode1 = "VARIÁVEL";
+const accessCode2 = "FUNÇÃO";
+const accessCode3 = "BANCO DE DADOS";
+
+function clues() {
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: Sempre mudando, não constante!", 100, 70);
+
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: Um trecho de código que realiza uma tarefa específica",600, 170);
+
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: Armazena Informações",100, 270);
+}
